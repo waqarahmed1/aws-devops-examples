92df957195a4c069900de4628894ecf64b48a7b9
Waqar Ahmed <waqarahmed@Waqars-MacBook-Pro.local>
2019-11-28 18:14:40 +0000

    Add example

---
+++ CodePipeline-Simple-Java/src/main/java/com/example/app/App.java
@@ -0,0 +1,19 @@
+package com.example.app;
+
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println(MessageOne());
+
+        System.out.println(MessageTwo());
+    }
+
+    public static String MessageOne() {
+        return "Hello World !!!";
+    }
+
+    public static String MessageTwo() {
+        return "CodePipeline Success !";
+    }
+
+}
---
+++ CodePipeline-Simple-Java/src/test/java/com/example/app/AppTest.java
@@ -0,0 +1,18 @@
+package com.example.app;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class AppTest {
+
+    @Test
+    public void appFirst() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void appSecond() {
+        Assert.assertFalse(false);
+    }
+}
