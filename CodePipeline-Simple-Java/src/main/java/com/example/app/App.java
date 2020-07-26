899ba87630b356b16421883061f6c692fcc2e8c4
Waqar Ahmed <waq70@hotmail.com>
2020-07-26 13:03:24 +0100

    Update App.java

--- CodePipeline-Simple-Java/src/main/java/com/example/app/App.java
+++ CodePipeline-Simple-Java/src/main/java/com/example/app/App.java
@@ -3,8 +3,8 @@ package com.example.app;
 
 public class App {
     public static void main(String[] args) {
+        
         System.out.println(MessageOne());
-
         System.out.println(MessageTwo());
     }
 
